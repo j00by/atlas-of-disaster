85a7eeae9d292215a5af723c73539f3d6d3d34ab
j00by <judy.h@nyu.edu>
2024-05-05 00:37:52 -0400

    try popup marker

--- scripts.js
+++ scripts.js
@@ -14,6 +14,83 @@ map.addControl(new mapboxgl.NavigationControl());
 
 
 map.on('load', function () {
+    // Information box top left for methodology
+    document.getElementById('info-icon').addEventListener('click', function () {
+        var infoPanel = document.getElementById('info-panel');
+        if (infoPanel.style.display === 'none') {
+            infoPanel.style.display = 'block';
+        } else {
+            infoPanel.style.display = 'none';
+        }
+    });
+    
+        // Load an image from an external URL
+        map.loadImage('https://ziplook.house.gov/zip/pictures/ky05_rogersharold.jpg', function (error, image) {
+            if (error) throw error;
+    
+            // Add the image to the map style
+            map.addImage('harold-rogers', image);
+    
+            // Add a data source containing one point feature
+            map.addSource('point', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': [
+                        {
+                            'type': 'Feature',
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': [-83.07464, 37.69088] // Coordinates for the marker in Kentucky
+                            }
+                        }
+                    ]
+                }
+            });
+    
+            // Add a layer to use the image to represent the data
+            map.addLayer({
+                'id': 'point-layer',
+                'type': 'symbol',
+                'source': 'point',
+                'layout': {
+                    'icon-image': 'harold-rogers', // reference the image by the ID
+                    'icon-size': 0.75 // Adjusts the size of the icon
+                }
+            });
+        });
+    
+        // Create a popup
+        var popup = new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`
+                <div style="width: 200px; text-align: center;">
+                    <h3>Harold Rogers</h3>
+                    <p>Kentucky</p>
+                </div>
+            `);
+    
+        // When the user clicks on the custom marker, open the popup
+        map.on('click', 'point-layer', function (e) {
+            popup.setLngLat(e.lngLat)
+                 .addTo(map);
+        });
+    
+        // Change the cursor to a pointer when the mouse is over the point layer
+        map.on('mouseenter', 'point-layer', function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+    
+        // Change it back to a default when it leaves
+        map.on('mouseleave', 'point-layer', function () {
+            map.getCanvas().style.cursor = '';
+        });
+
+
+
+
+
+
+        
     // Load the GeoJSON file for congressional districts with representative names
     map.addSource('districts', {
         type: 'geojson',
@@ -148,15 +225,6 @@ map.on('mouseleave', 'counties-layer', function () {
 
 
 
-    // Information box top left for methodology
-    document.getElementById('info-icon').addEventListener('click', function () {
-        var infoPanel = document.getElementById('info-panel');
-        if (infoPanel.style.display === 'none') {
-            infoPanel.style.display = 'block';
-        } else {
-            infoPanel.style.display = 'none';
-        }
-    });
     
-
+    
 });
